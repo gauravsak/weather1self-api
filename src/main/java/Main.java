b92d5d75b2bd6412430d764b43180b89d9ddaf63
Gaurav Sakhardande <sakhardande.gaurav@gmail.com>
2015-03-22 20:06:43 +0000

    Removed POST handler from configuration and fixed the test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,7 @@
 import com.equalexperts.weather1self.client.Lib1SelfClient;
 import com.equalexperts.weather1self.model.lib1self.Stream;
 import com.equalexperts.weather1self.model.lib1self.WeatherSource;
-import com.equalexperts.weather1self.server.PostHandler;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.HandlerCollection;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
@@ -13,22 +11,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.sql.*;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
 
-//        if (req.getRequestURI().endsWith("/db")) {
-//            showDatabase(req, resp);
-//        } else {
 //            showHome(req, resp);
-//        }
 
         Map<String, String> queryParams = getQueryParams(req.getQueryString());
         String city = queryParams.get("city");
@@ -70,45 +63,6 @@ public class Main extends HttpServlet {
         resp.getWriter().print("Hello from Java!");
     }
 
-    private void showDatabase(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        Connection connection = null;
-        try {
-            connection = getConnection();
-
-            Statement stmt = connection.createStatement();
-            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-            stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-            ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-            String out = "Hello!\n";
-            while (rs.next()) {
-                out += "Read from DB: " + rs.getTimestamp("tick") + "\n";
-            }
-
-            resp.getWriter().print(out);
-        } catch (Exception e) {
-            resp.getWriter().print("There was an error: " + e.getMessage());
-        } finally {
-            if (connection != null) try {
-                connection.close();
-            } catch (SQLException e) {
-            }
-        }
-    }
-
-    private Connection getConnection() throws URISyntaxException, SQLException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        int port = dbUri.getPort();
-
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ":" + port + dbUri.getPath();
-
-        return DriverManager.getConnection(dbUrl, username, password);
-    }
-
     public static void main(String[] args) throws Exception {
         String port = System.getenv("PORT") == null ? "9000" : System.getenv("PORT");
         Server server = new Server(Integer.valueOf(port));
@@ -116,11 +70,7 @@ public class Main extends HttpServlet {
         ServletContextHandler defaultContext = new ServletContextHandler(ServletContextHandler.SESSIONS);
         defaultContext.setContextPath("/");
         defaultContext.addServlet(new ServletHolder(new Main()), "/*");
-
-        HandlerCollection handlers = new HandlerCollection();
-        handlers.addHandler(new PostHandler());
-        handlers.addHandler(defaultContext);
-        server.setHandler(handlers);
+        server.setHandler(defaultContext);
 
         server.start();
         server.join();
--- src/main/test/com/equalexperts/weather1self/GetTemperatureChartTest.java
+++ src/main/test/com/equalexperts/weather1self/GetTemperatureChartTest.java
@@ -2,7 +2,7 @@ package com.equalexperts.weather1self;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
@@ -14,13 +14,12 @@ public class GetTemperatureChartTest {
 
     @Test
     public void getsTemperatureChart() throws IOException {
-
         CloseableHttpClient httpClient = HttpClients.createDefault();
-//        HttpPost httpPost = new HttpPost("http://weather1self-api.heroku.com/");
-        HttpPost httpPost = new HttpPost("http://localhost:9000?city=Pune&country=IN");
-        httpPost.addHeader("x-weather-source", "wunderground.com");
+//        HttpGet temperatureChartGET = new HttpGet("http://weather1self-api.heroku.com/");
+        HttpGet temperatureChartGET = new HttpGet("http://localhost:9000?city=Pune&country=IN");
+        temperatureChartGET.addHeader("x-weather-source", "wunderground.com");
 
-        try (CloseableHttpResponse response = httpClient.execute(httpPost)) {
+        try (CloseableHttpResponse response = httpClient.execute(temperatureChartGET)) {
             System.out.println(response.getStatusLine());
             HttpEntity entity = response.getEntity();
             System.out.println(EntityUtils.toString(entity));
