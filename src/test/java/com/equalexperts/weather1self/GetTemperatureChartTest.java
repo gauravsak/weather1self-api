7a60ee6f56e1c77d3264184f6f688ca550162032
Gaurav Sakhardande <sakhardande.gaurav@gmail.com>
2015-03-25 13:10:32 +0000

    Fixed test

--- src/test/java/com/equalexperts/weather1self/GetTemperatureChartTest.java
+++ src/test/java/com/equalexperts/weather1self/GetTemperatureChartTest.java
@@ -15,7 +15,7 @@ public class GetTemperatureChartTest {
     @Test
     public void getsTemperatureChart() throws IOException {
         CloseableHttpClient httpClient = HttpClients.createDefault();
-        HttpGet temperatureChartGET = new HttpGet("http://weather1self-api.heroku.com/");
+        HttpGet temperatureChartGET = new HttpGet("http://weather1self-api.heroku.com?city=Pune&country=IN");
 //        HttpGet temperatureChartGET = new HttpGet("http://localhost:9000?city=Pune&country=IN");
         temperatureChartGET.addHeader("x-weather-source", "wunderground.com");
 
