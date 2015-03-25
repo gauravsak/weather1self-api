cdf03c0f32e16c486d340d24f08971b73b231cf1
Gaurav Sakhardande <sakhardande.gaurav@gmail.com>
2015-03-25 13:07:45 +0000

    Pointing the test to app depoyed on heroku

--- src/test/java/com/equalexperts/weather1self/GetTemperatureChartTest.java
+++ src/test/java/com/equalexperts/weather1self/GetTemperatureChartTest.java
@@ -15,8 +15,8 @@ public class GetTemperatureChartTest {
     @Test
     public void getsTemperatureChart() throws IOException {
         CloseableHttpClient httpClient = HttpClients.createDefault();
-//        HttpGet temperatureChartGET = new HttpGet("http://weather1self-api.heroku.com/");
-        HttpGet temperatureChartGET = new HttpGet("http://localhost:9000?city=Pune&country=IN");
+        HttpGet temperatureChartGET = new HttpGet("http://weather1self-api.heroku.com/");
+//        HttpGet temperatureChartGET = new HttpGet("http://localhost:9000?city=Pune&country=IN");
         temperatureChartGET.addHeader("x-weather-source", "wunderground.com");
 
         try (CloseableHttpResponse response = httpClient.execute(temperatureChartGET)) {
